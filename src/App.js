9b64e38149174fa47de71a2bb93b06f68b24d987
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-08-22 14:00:25 +0700

    create simple counter with React Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
+import "./App.css";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Tambah
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Kurang
+      </button>
     </div>
   );
 }
