278fe5ad90e5dbd3a2f61ea3d3234c1122a2f014
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-09-03 21:49:03 +0700

    style: add search input form

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ function App() {
         <img src={shoppingicon} alt="icon" className="nav-icon" />
         <h1 className="nav-title">Shopping List</h1>
       </nav>
+
+      <section className="container">
+        <from className="form">
+          <input className="input" type="text" placeholder="Input your list" />
+          <button className="add-button" type="submit">
+            add
+          </button>
+        </from>
+      </section>
     </>
   );
 }
