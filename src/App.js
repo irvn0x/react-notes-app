0f4cda627cae507317811ded0b1d869268787667
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-09-06 11:47:43 +0700

    style: add search input components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import minusIcon from "./assets/minus-icon.svg";
 
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
+import SearchInput from "./components/SearchInput";
 
 function App() {
   const [value, setValue] = useState("");
@@ -75,21 +76,13 @@ function App() {
     <>
       <Navbar />
       <Container>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-            value={value}
-            className="input"
-            type="text"
-            placeholder="Input your list"
-          />
-          <button className="add-button" type="submit">
-            add
-          </button>
-        </form>
-
+        <SearchInput
+          onSubmit={handleSubmit}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          value={value}
+        />
         <div className="info">
           <div className="info-total">
             <p>{`Total List: ${todos.length}`}</p>
